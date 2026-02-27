5d44c5f1c567ef6f449e60d0d7558245273e6b49
ONS0BAHRI <bahri.ons@esprit.tn>
2026-02-27 05:53:52 +0100

    Remove build/log/install from tracking, add .gitignore

--- Camera_D435i/build/mqtt_rgb_bridge/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/turtlebot1/ros2_ws/src/mqtt_rgb_bridge
-# Build directory: /home/turtlebot1/ros2_ws/build/mqtt_rgb_bridge
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
